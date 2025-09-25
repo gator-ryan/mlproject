423eb6f40b6aa0f02ac199074f2c9efcc6b65d21
Narayan Singh Bhadauriya <professionalsinghbhadauriya@gmail.com>
2025-09-25 16:46:31 +0530

    Model Training & best model finding

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,18 +11,20 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
-# from src.components.model_trainer import ModelTrainerConfig
-# from src.components.model_trainer import ModelTrainer
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
     test_data_path: str=os.path.join('artifacts',"test.csv")
     raw_data_path: str=os.path.join('artifacts',"data.csv")
+    ## these above are the path where we want to save our data
 
 class DataIngestion:
     def __init__(self):
         self.ingestion_config=DataIngestionConfig()
+        ## consist of all the path we intend to use in data ingestion
 
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
@@ -41,7 +43,7 @@ class DataIngestion:
 
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
 
-            logging.info("Inmgestion of the data iss completed")
+            logging.info("Ingestion of the data is completed")
 
             return(
                 self.ingestion_config.train_data_path,
@@ -58,8 +60,8 @@ if __name__=="__main__":
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
-    # modeltrainer=ModelTrainer()
-    # print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
 
 
 
